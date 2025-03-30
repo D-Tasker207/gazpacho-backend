d6c6542b208fc4c7cbf6c9da74381922a97f88f0
1-Megan-1 <moconnor3@ithaca.edu>
2025-03-30 18:33:05 -0400

    RecipeServiceApplication implementation

--- recipe-service/src/main/java/com/gazpacho/recipeservice/RecipeServiceApplication.java
+++ recipe-service/src/main/java/com/gazpacho/recipeservice/RecipeServiceApplication.java
@@ -1,5 +1,11 @@
 package com.gazpacho.recipeservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RecipeServiceApplication {
-    //Spring boot entry point
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeServiceApplication.class, args);
+    }
 }
