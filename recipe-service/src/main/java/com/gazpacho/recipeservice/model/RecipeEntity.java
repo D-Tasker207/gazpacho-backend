7096d458ce4ec8ca54f4e442638dae4cd9ce4d24
1-Megan-1 <moconnor3@ithaca.edu>
2025-03-30 18:47:11 -0400

    RecipeEntity and placeholder IngredientEntity

--- recipe-service/src/main/java/com/gazpacho/recipeservice/model/RecipeEntity.java
+++ recipe-service/src/main/java/com/gazpacho/recipeservice/model/RecipeEntity.java
@@ -1,5 +1,39 @@
 package com.gazpacho.recipeservice.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "recipes", 
+       indexes = { @Index(name = "idx_recipe_name", columnList = "name") })
 public class RecipeEntity {
-    //Persisting recipe entries into db eventually
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    //Many to many ingredient-recipe relationship
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "recipe_ingredients",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "ingredient_id")
+    )
+    private Set<IngredientEntity> ingredients = new HashSet<>();
+
+    //Currently assuming steps are a list of strings
+    @ElementCollection
+    @CollectionTable(name = "recipe_steps", joinColumns = @JoinColumn(name = "recipe_id"))
+    @Column(name = "step")
+    private List<String> steps = new ArrayList<>();
 }
