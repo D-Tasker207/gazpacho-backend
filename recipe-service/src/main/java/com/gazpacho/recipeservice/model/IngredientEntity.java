65a3188f3e5a7ab6d034483fc3753fd9121e1524
1-Megan-1 <moconnor3@ithaca.edu>
2025-04-01 11:50:12 -0400

    Updated IngredientEntity

--- recipe-service/src/main/java/com/gazpacho/recipeservice/model/IngredientEntity.java
+++ recipe-service/src/main/java/com/gazpacho/recipeservice/model/IngredientEntity.java
@@ -1,22 +1,24 @@
 package com.gazpacho.recipeservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-//TODO: Finalize actual ingredient entity, this is a placeholder for recipe entity
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
 @Entity
-@Table(name = "ingredients")
+@Table(name = "ingredients", indexes = { @Index(name = "idx_ingredient_name", columnList = "name") })
 public class IngredientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    // A minimal placeholder field
+
     private String name;
+
+    //One-to-many relationship to IngredientAllergen join entity
+    //(Commented out until I add the join entity)
+    //@OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL, orphanRemoval = true)
+    //private Set<IngredientAllergen> ingredientAllergens = new HashSet<>();
 }
