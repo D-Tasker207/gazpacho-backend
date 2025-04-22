4c98726dc1124f6c1455d5ea3e689ca34c31d409
WhirlwindWar <gaglianopa@gmail.com>
2025-04-22 10:35:43 -0400

    fix cors

--- api-gateway/src/main/java/com/gazpacho/apigateway/config/CorsGlobalConfig.java
+++ api-gateway/src/main/java/com/gazpacho/apigateway/config/CorsGlobalConfig.java
@@ -12,7 +12,7 @@ public class CorsGlobalConfig {
   @Bean
   public CorsWebFilter corsWebFilter() {
     CorsConfiguration config = new CorsConfiguration();
-    config.addAllowedOrigin("loclalhost:3000");
+    config.addAllowedOrigin("http://localhost:3000");
     config.addAllowedMethod("*");
     config.addAllowedHeader("*");
     config.setAllowCredentials(true);
